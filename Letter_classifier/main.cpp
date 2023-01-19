236782681c5109b33316a98e650a830b6fa2848a
nickc <nickcig1@gmail.com>
2023-01-19 15:56:35 +0500

    Комментарий

--- Letter_classifier/main.cpp
+++ Letter_classifier/main.cpp
@@ -12,6 +12,7 @@ int main()
     //for (int i = 0; i < DATA.size_for_one_letter * 5 * 50 * 50; i++)
     //    std::cout << DATA.data[i];
 
+    //загрузка обучающей выборки в нейросеть
     cl.selection_load(DATA);
 
     std::vector<int> im(2500, 0);
